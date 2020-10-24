85744d337d2cac00abeae902fc0c728bb5083fd5
hotaru <me@whitedragon.ga>
2020-10-24 18:37:20 +0300

    commit

--- main.py
+++ main.py
@@ -2,27 +2,23 @@ import os, random, time
 import telebot
 #imports
 
-workfolder = "/path/to/images/folder"
-timetospeep = 60
-token = "yourbottoken"
-channelid = "yourchannelid"
+workfolder = "/path/to/your/folder" #полный путь к папке
+timetospeep = 1800 #промежуток между постами в секундах
+token = "" #Токен бота
+channelid = "" #айди канала где бот в админах
 #variables
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет, я бот для автопостинга пикчей в Telegram канал, чтобы я начал высылать пикчи, отредактируй переменные в коде, добавь меня в админы канала, и выстави айди канала в коде.")
 
-)
-def send_post(message):
-		pic = random.choice(os.listdir(workfolder))
-		fullpath = (workfolder + pic	
-	bot.send_photo(channelid, fullpath)
+while True:
+	pic = random.choice(os.listdir(workfolder))
+	fullpath = (workfolder + "/" + pic)
+	photo = open(fullpath, 'rb')
+	bot.send_photo(channelid, photo)
 	time.sleep(timetospeep)
 
 bot.polling()
-
-
-
-
